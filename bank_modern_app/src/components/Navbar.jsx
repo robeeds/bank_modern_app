954e2f27a538311d57bf9358d870ed3aa47b8dc3
RobertuhBruh <robeeds14@gmai.com>
2023-12-30 19:44:12 -0800

    first commit

---
+++ bank_modern_app/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import {useState } from 'react'
+import { close, logo, menu } from '../assets';
+import { navLinks } from '../constants';
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
---
+++ readme.md
@@ -0,0 +1,2 @@
+https://www.youtube.com/watch?v=_oO4Qi5aVZs
+Paused 31:27
