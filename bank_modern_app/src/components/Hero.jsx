92bb8c993aa0a3bb865445d01b679915f2823a13
robeeds <71895118+RobeeDS@users.noreply.github.com>
2024-01-02 20:18:10 -0800

    adjusted classNames for home section

--- bank_modern_app/src/components/Hero.jsx
+++ bank_modern_app/src/components/Hero.jsx
@@ -6,8 +6,8 @@ const Hero = () => {
 
   //Unlike the video at 47:44, the discount logo will not show unless there is a return statement
   return (
-    <section id="home" className={`flex md: flex-row flex-col ${styles.paddingY}`}>
-    <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm: px-16 px-6`} >
+    <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
+    <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`} >
       <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
         <img src={discount} alt="discount" className="w-[32px] h-[32px]"/>
         <p className={`${styles.paragraph} ml-2`}>
