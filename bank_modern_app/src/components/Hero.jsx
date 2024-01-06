cbecda06ad7fcf591800c654c18cbee8e94ae9dc
RobertuhBruh <robeeds14@gmai.com>
2024-01-05 23:39:32 -0800

    Finished Hero

--- bank_modern_app/src/components/Hero.jsx
+++ bank_modern_app/src/components/Hero.jsx
@@ -4,7 +4,7 @@ import GetStarted from './GetStarted';
 
 const Hero = () => {
 
-  //Unlike the video at 47:44, the discount logo will not show unless there is a return statement
+  //Unlike the video at 47:44, the discount logoa and other components will not show unless there is a return statement
   return (
     <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
     <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`} >
@@ -29,9 +29,21 @@ const Hero = () => {
       </div>
 
       <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100px] leading-[75px]" w-full>Payment Method.</h1>
-      <p></p>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}> Our team of experts uses a methodology to identify the credit cards most likey to fit your needs. We examine annual percentage rates, annual fees, and much more.</p>
+      </div>
 
-    </div>
+      <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
+        <img src={robot} alt="Billing" className="w-[100%] h-[100%] relative z-[5]"/>
+
+        <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
+        <div className="absolute z-[1] w-[80%] h-[80%] rounded-full bottom-40 white__gradient" />
+        <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
+      </div>
+
+      <div className={`ss:hidden ${styles.flexCenter}`}>
+        <GetStarted />
+      </div>
+
   </section>
   )
 }
--- readme.md
+++ readme.md
@@ -1,2 +1,3 @@
 https://www.youtube.com/watch?v=_oO4Qi5aVZs
 Paused 58:36 Hero Section
+ADD reset.css, site shows differently in various browsers
